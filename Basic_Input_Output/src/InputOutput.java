4dcefe88d1f95677dd2a061e82f23a0ea7468e93
bhumi <bhumi.rastogi05@gmail.com>
2024-02-11 23:59:48 +0530

    basic input output completed

--- Basic_Input_Output/src/InputOutput.java
+++ Basic_Input_Output/src/InputOutput.java
@@ -2,8 +2,55 @@
 public class InputOutput {
 
 	public static void main(String[] args) {
-		
+// Output
+    	System.out.println(10);
+    	System.out.println(7);
 
+    	
+// ln uses
+    	System.out.print(10);
+    	System.out.println(7);
+    	System.out.println(8);
+
+    	
+// Multiply
+    	System.out.print(10*7);
+    	
+    	
+// Double quote printing
+    	System.out.println("AlgoPrep");
+    	
+  
+// Plus Sign in output
+    	System.out.println(7*10 + "Hello");
+    	
+    	
+//Declaring the variables
+    	int x;
+    	x = 30;
+    	System.out.println(x);
+
+    	
+// Using Variables
+    	int temp;
+    	temp = 20;
+    	System.out.println(temp);
+    	temp = 100;
+    	System.out.println(temp);
+    	
+    	
+//Same variable can't be created more than once
+    	int all;
+    	all = 1000;
+    	System.out.println(all);
+//    	int all;
+    	all = 20;
+    	System.out.println(all);
+    
+    	
+// one variable line
+    	int cho = 900;
+    	System.out.println(cho);
 	}
 
 }

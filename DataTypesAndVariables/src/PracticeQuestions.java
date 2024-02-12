e2d87997781d812095b0de9ec23059de1f22d92e
bhumi <bhumi.rastogi05@gmail.com>
2024-02-12 14:16:09 +0530

    doing practice

---
+++ DataTypesAndVariables/src/PracticeQuestions.java
@@ -0,0 +1,58 @@
+
+public class PracticeQuestions {
+
+	public static void main(String[] args) {
+// Practice Question
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+		
+		
+		System.out.println(a+" "+b);
+		
+		
+		int temp = 100;
+//		System.out.println(True);
+		
+		
+//		System.out.println(a);
+//		int a = 100;
+		
+		
+//		int a = 200;
+//		int a =20000;
+//		System.out.println(a);
+		
+		
+		int x = 20;
+		System.out.println(x);
+		x = 40;
+		System.out.println(x);
+		
+		
+		int z = 10;
+		int y = 20;
+		z = y + 30;
+		System.out.println(z);
+		
+
+//		int m = 10;
+//		int 1y = 20;
+//		int y@b=30;
+		
+		
+//		int _y = 10;
+//		int xxy = 20;
+//		int x  a = 30;
+//		int y$z = 45;
+		
+		
+//		int static = 10;
+//		System.out.println(static);
+		
+		
+		int Static = 10;
+		System.out.println(Static);
+	}
+
+}
